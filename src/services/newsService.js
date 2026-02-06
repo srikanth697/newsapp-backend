fb678fa97c78f6530f2ecc7345398e209bd608e0
srikanthparimisetty <srikanthparimisetty93@gmail.com>
2026-02-06 17:06:38 +0530

    Simplify news fetch logic to guarantee data insertion

--- src/services/newsService.js
+++ src/services/newsService.js
@@ -3,33 +3,28 @@ import News from "../models/News.js";
 
 export const fetchIndiaNews = async () => {
     const response = await axios.get(
-        "https://newsapi.org/v2/top-headlines",
+        "https://newsapi.org/v2/everything?q=india",
         {
             params: {
-                country: "in",
                 apiKey: process.env.NEWS_API_KEY,
             },
         }
     );
 
+    await News.deleteMany({}); // clear old data (TEMP)
+
     for (const article of response.data.articles) {
         if (!article.title) continue;
 
-        await News.updateOne(
-            { title: article.title },
-            {
-                title: article.title,
-                description: article.description,
-                content: article.content,
-                image: article.urlToImage,
-                sourceUrl: article.url,
-
-                country: "IN",
-                category: "general", // Default for top-headlines if category not specified
-
-                publishedAt: article.publishedAt,
-            },
-            { upsert: true }
-        );
+        await News.create({
+            title: article.title,
+            description: article.description,
+            content: article.content,
+            image: article.urlToImage,
+            sourceUrl: article.url,
+            country: "IN",
+            category: "general",
+            publishedAt: article.publishedAt,
+        });
     }
 };
